ef1032657379c10481be91107a329dbac2a0389a
gavin-shaw <gavin@gnarus.io>
2022-03-25 14:16:10 +0200

    feat: add earnkeeper theme
    Default to dark mode
    Add Earn Keeper Logo
    Use Earn Keeper color palette

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,32 +1,32 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'EarnKeeper',
-  tagline: 'Play To Earn Tools',
-  url: 'https://developers.earnkeeper.io',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'earnkeeper', // Usually your GitHub org/user name.
-  projectName: 'earnkeeper', // Usually your repo name.
+  title: "EarnKeeper",
+  tagline: "Play To Earn Tools",
+  url: "https://developers.earnkeeper.io",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "earnkeeper", // Usually your GitHub org/user name.
+  projectName: "earnkeeper", // Usually your repo name.
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/earnkeeper/docs',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/earnkeeper/docs",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -36,66 +36,65 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'EarnKeeper',
+        title: "EarnKeeper",
         logo: {
-          alt: 'EarnKeeper Logo',
-          src: 'img/logo.svg',
+          alt: "EarnKeeper Logo",
+          src: "img/logo.png",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'user-guide/welcome',
-            position: 'left',
-            label: 'User Guide',
+            type: "doc",
+            docId: "user-guide/welcome",
+            position: "left",
+            label: "User Guide",
           },
           {
-            type: 'doc',
-            docId: 'developer/welcome',
-            position: 'left',
-            label: 'Developer',
+            type: "doc",
+            docId: "developer/welcome",
+            position: "left",
+            label: "Developer",
           },
           {
-            href: 'https://github.com/earnkeeper/ekp-sdk',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/earnkeeper/ekp-sdk",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'User Guide',
-                to: '/docs/user-guide/welcome',
+                label: "User Guide",
+                to: "/docs/user-guide/welcome",
               },
               {
-                label: 'Developer',
-                to: '/docs/developer/welcome',
+                label: "Developer",
+                to: "/docs/developer/welcome",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Discord',
-                href: 'https://discord.gg/92EMAWVrrn',
+                label: "Discord",
+                href: "https://discord.gg/92EMAWVrrn",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/earnkeeper',
+                label: "Twitter",
+                href: "https://twitter.com/earnkeeper",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/earnkeeper',
+                label: "GitHub",
+                href: "https://github.com/earnkeeper",
               },
             ],
           },
@@ -105,6 +104,9 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+      },
+      colorMode: {
+        defaultMode: "dark",
       },
     }),
 };
